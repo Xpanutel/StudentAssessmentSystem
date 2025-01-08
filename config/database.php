e0a040763eed221a824f52504cc76d5fb00396ab
Xpanutel <fedpet75@gmail.com>
2025-01-08 17:33:24 +0300

    создание шаблона интерфейса пользователя на html

--- config/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$host = "localhost"; // тут меняешь на своё
-$username = "root"; // тут меняешь на своё
-$password = ""; // тут меняешь на своё
-$dbname = "StudentAssessmentSystem"; // тут меняешь на своё
